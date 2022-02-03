eebecbaa9589e8114197706237a3036cffe45eba
audiBookning <audiobookning@gmail.com>
2022-02-03 06:17:02 +0000

    correction for typescript does not build d.ts
    correction for typescript does not build d.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
 import 'dotenv/config'
-import { Account } from './lib/account.js'
 import { Client } from './lib/client.js'
 import { Games } from './lib/games.js'
-import { Relations } from './lib/to-implement/relations.js'
+import { Relations } from './lib/relations.js'
 import { Users } from './lib/users.js'
+import { Lichess } from './lichess.js'
 
-function Lichess(token: string) {
-  const client = Client(token)
-  return {
-    account: Account(client),
-    games: Games(client),
-    relations: Relations(client),
-    users: Users(client),
-  }
-}
-
-export { Lichess }
+// export Functions
+export { Lichess, Client, Games, Relations, Users }
---
+++ src/lichess.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import { Account } from './lib/account.js'
+import { Client } from './lib/client.js'
+import { Games } from './lib/games.js'
+import { Relations } from './lib/relations.js'
+import { Users } from './lib/users.js'
+
+function Lichess(token: string) {
+  const client = Client(token)
+  return {
+    account: Account(client),
+    games: Games(client),
+    relations: Relations(client),
+    users: Users(client),
+  }
+}
+
+export { Lichess }
